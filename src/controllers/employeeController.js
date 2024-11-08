eaca5d6c5b457ee11413978281c56daa52f90225
Asif-102 <asif1zero2@gmail.com>
2024-11-09 02:17:03 +0600

    get all chidrens data using parent id

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -2,6 +2,30 @@ const db = require("../models");
 
 const Employee = db.employees;
 
+const getChildren = async (parentId) => {
+  const children = await Employee.findAll({
+    where: { parentId },
+    order: [["positionId", "ASC"]],
+  });
+
+  if (children.length > 0) {
+    return Promise.all(
+      children.map(async (child) => {
+        const childData = {
+          id: child.id,
+          name: child.name,
+          positionId: child.positionId,
+          positionName: child.positionName,
+          child: await getChildren(child.id),
+        };
+        return childData;
+      })
+    );
+  }
+
+  return null;
+};
+
 const createEmployee = async (req, res) => {
   try {
     const newEmployee = await Employee.create(req.body);
@@ -18,4 +42,23 @@ const createEmployee = async (req, res) => {
   }
 };
 
-module.exports = { createEmployee };
+const getEmployeeById = async (req, res) => {
+  try {
+    const employeeId = req.params.id;
+    const employee = await Employee.findByPk(employeeId);
+
+    if (!employee) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+
+    const allChildrenData = await getChildren(employeeId);
+
+    const result = allChildrenData.filter((child) => child.id !== employeeId);
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { createEmployee, getEmployeeById };
--- src/routers/employeeRouter.js
+++ src/routers/employeeRouter.js
@@ -4,5 +4,6 @@ const auth = require("../middleware/auth");
 const router = require("express").Router();
 
 router.post("/createEmployee", auth, employeeController.createEmployee);
+router.get("/:id", auth, employeeController.getEmployeeById);
 
 module.exports = router;
